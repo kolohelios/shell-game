34c6197de9a0f3c3a6fdc5c7b099656506b8c54d
Jon Edwards <jkedwards@me.com>
2015-04-08 17:24:34 -0700

    adding functionality

--- client/index.js
+++ client/index.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init() {
+  $('#begin').click(begin);
+}
+
+function begin() {
+  var amount = $('#amount').val() * 1;
+  var ball = Math.floor(Math.random()*amount);
+  console.log(ball);
+  $('#cups').empty();
+  for(var i = 0; i < amount; i++) {
+    var $cup = $('<div>');
+    $cup.addClass('cup');
+    $('#cups').append($cup);
+  }
+}
